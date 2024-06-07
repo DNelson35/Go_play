595147b29db74d3b200b6f4ab3ce6268acf8a610
Damien Nelson <dhnisaman@gmail.com>
2024-06-06 22:49:01 -0400

    set up greeting function to handle and return error if name is not provided

--- go_playground/greetings/greetings.go
+++ go_playground/greetings/greetings.go
@@ -1,8 +1,13 @@
 package greetings
 
-import "fmt"
-
-func Greeting(name string) string {
+import ( 
+	"errors"
+	"fmt"
+)
+func Greeting(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name must not be empty")
+	}
 	message := fmt.Sprintf("hi %v. Welcome!", name)
-	return message
+	return message, nil
 }
