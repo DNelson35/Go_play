27d5a7dd2d61dfb865324999f6432b2abecfc8ab
Damien Nelson <dhnisaman@gmail.com>
2024-06-06 22:50:39 -0400

    set up std logger to output error and exit program in the case greeting returns an error message.

--- go_playground/Hello/hello.go
+++ go_playground/Hello/hello.go
@@ -6,16 +6,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 
-var name string = "Damien"
+// var name string = "Damien"
+
 func main() {
 	// fmt.Printf("Hello, World! My name is '%s'\n", name)
 	// fmt.Println(quote.Go())
-	message := greetings.Greeting(name)
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Greeting("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(message)
 }
