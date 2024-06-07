f80370460caa1395fff0a74a40a6a02d23dcfc03
Damien Nelson <dhnisaman@gmail.com>
2024-06-06 23:17:04 -0400

    import rand and add a slice with diff strings to return diff greetings when called

--- go_playground/greetings/greetings.go
+++ go_playground/greetings/greetings.go
@@ -3,11 +3,23 @@ package greetings
 import ( 
 	"errors"
 	"fmt"
+	"math/rand"
 )
 func Greeting(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("name must not be empty")
 	}
-	message := fmt.Sprintf("hi %v. Welcome!", name)
+	message := fmt.Sprintf(randFormat(), name)
 	return message, nil
+}
+
+func randFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+		"%v, who is you?",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
