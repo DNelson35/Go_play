f69806c8ed7a2ce1d9c5d61d40b5568814352a33
Damien Nelson <dhnisaman@gmail.com>
2024-06-09 14:22:11 -0400

    update with comments and add a map of questions.

--- go_playground/mini_projects/quiz/quiz.go
+++ go_playground/mini_projects/quiz/quiz.go
@@ -9,44 +9,59 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to my quiz game!")
-	fmt.Printf("Would you like to play (y/n)? ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
+	fmt.Println("Welcome to my quiz game!") // Println adds newline at the end
+	fmt.Printf("Would you like to play (y/n)? ") // printf allows variables and continues in same line
+	reader := bufio.NewReader(os.Stdin) // look up how the buffer works and what the reder is and how and why this is better than simply scanning in text form stdin
+	input, err := reader.ReadString('\n') // telling the reader to read stdin up untill new line
 	if err!= nil {
-		fmt.Println("Error reading input:", err)
+		fmt.Println("Error reading input:", err) // if error print message quit program
 		return
 	}
 	input = strings.TrimSpace(input) // Remove trailing newline
 
-	if input == "n" || input == "" {
+	if strings.ToLower(input) != "y" { // exit program if they don't answer yes
 		fmt.Println("\nSorry to hear that. Maybe next time, goodbye.")
 		return
 	}
 
+	questions := map[string]string{
+		"how many moons does Earth have? ": "1",
+		"how long is a day in hours? ": "24",
+		"who is the flash? ": "barry alan",
+		"what year was the declaration of independence signed? ": "1776",
+	}
+
 	fmt.Println("Awesome, let's get started...")
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(2 * time.Second) // if yes we print message wait 2 seconds then clear screen
 
 	clearScreen()
 
-	fmt.Printf("Question 1: Who am I? ")
-	input, err = reader.ReadString('\n')
-	if err!= nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-	input = strings.TrimSpace(input) // Remove trailing newline
+	for question, answer := range questions{
+		fmt.Printf("%v", question)
+		input, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+			time.Sleep(time.Second * 2)
+			return
+		}
 
-	if input != "Damien" {
-		fmt.Printf("Sorry, %s is incorrect.\n", input)
-		fmt.Printf("Try again next time...\n")
+		input = strings.TrimSpace(input)
+		
+
+		if strings.ToLower(input) != answer {
+			fmt.Printf("Sorry, %s is incorrect.\n", input)
+			fmt.Printf("Try again next time...\n")
+			time.Sleep(2 * time.Second)
+			return
+		}
+
+		fmt.Println("You answered correctly.")
 		time.Sleep(2 * time.Second)
-		return
+		clearScreen()
+		
 	}
 
-	fmt.Println("You answered correctly.")
-	// Add logic here to continue with the next question or exit the game
 }
 
 func clearScreen() {
