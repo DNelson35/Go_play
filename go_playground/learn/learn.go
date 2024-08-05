3dc836f74d7829d3645f9952ce2af982b51e2135
Damien Nelson <dhnisaman@gmail.com>
2024-08-05 02:35:39 -0400

    clean up comments

--- go_playground/learn/learn.go
+++ go_playground/learn/learn.go
@@ -3,41 +3,19 @@ package main
 import (
 	"fmt"
 	"learn/utils"
-	// "path/filepath"
-	// "strings"
-	// "os"
-	// "io"
 )
-
-// var count = utils.WordCount("the one who knows one is one who knows none")
 
 
 func main(){
 	
-	// fmt.Println(utils.Primefactor(600851475143))
+
 	// fmt.Println(utils.LgPalindrom())
 	// fmt.Println(utils.FindDiffSquares(100))
 
-	// var nums = []int{2,-1,1}
-
 	// fmt.Println(utils.GetPrime(10001))
 
-	// ".*?" ==> To(Equal(false)) ✔️
-	// "a" ==> To(Equal(true)) ✔️
-	// "Mazinkaiser" ==> To(Equal(true)) ✔️
-	// "hello world_" ==> To(Equal(false)) ✔️
-	// "     " ==> To(Equal(false)) ✔️
-	// "PassW0rd" ==> To(Equal(true)) ✔️
-	// "" ==> To(Equal(false)) ✔️
-	// "\n\t\n" ==> To(Equal(false)) ✔️
-	// "ciao\n$$_" ==> To(Equal(false)) ✔️
-	// "__ * __" ==> To(Equal(false)) ✔️
-	// "&)))(((" ==> To(Equal(false)) ✔️
-	// "43534h56jmTHHF3k" ==> To(Equal(true)) ✔️
+	count := utils.WordCount("the one who knows one is one who knows none")
 
-	fmt.Println(utils.Alphanumeric(""))
-
-	
-
+	fmt.Println(count)
 
 }
